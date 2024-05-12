ee0ca6957d457b351d890775b7e83789eb261a16
Susmith A J <susmith508@gmail.com>
2024-05-12 19:30:18 +0530

    counter app

---
+++ src/Counter.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [signedIn, setSignedIn] = useState(false)
+
+    const handleSignIn = () => {
+        setSignedIn(true)
+    }
+
+    const handleSignOut = () => {
+        setSignedIn(false)
+    }
+
+    const [count, setCount] = useState(0)
+
+    const increamentCounter = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            { signedIn ? (
+                <div>
+                    <button type='button' onClick={handleSignOut}> Sign Out </button>
+                    <p>Welcome Back!</p>
+
+                    <button type='button' onClick={increamentCounter}> You clicked {count} times.</button>
+
+                </div>) : (
+                    <div>
+                        <button type='button' onClick={handleSignIn}> Sign In </button>
+                        <p>Please Sign In!</p>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default Counter;
